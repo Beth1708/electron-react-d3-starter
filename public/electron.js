d464927c401879b91f2af4277be53c7923d23adb
Elizabeth Kirby <bethk@uw.edu>
2021-07-31 14:40:50 -0700

    Second window shows up

--- public/electron.js
+++ public/electron.js
@@ -1,74 +1,117 @@
 const path = require("path");
 
-const { app, BrowserWindow } = require("electron");
+const {app, BrowserWindow, ipcMain} = require("electron");
 const isDev = require("electron-is-dev");
+
+const {ipcConstants} = require(path.join(app.getAppPath(), './src/utils/IpcConstants.js'));
 
 // Conditionally include the dev tools installer to load React Dev Tools
 let installExtension, REACT_DEVELOPER_TOOLS;
 
 if (isDev) {
-  const devTools = require("electron-devtools-installer");
-  installExtension = devTools.default;
-  REACT_DEVELOPER_TOOLS = devTools.REACT_DEVELOPER_TOOLS;
+    const devTools = require("electron-devtools-installer");
+    installExtension = devTools.default;
+    REACT_DEVELOPER_TOOLS = devTools.REACT_DEVELOPER_TOOLS;
 }
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling
 if (require("electron-squirrel-startup")) {
-  app.quit();
+    app.quit();
 }
 
+let mainWindow;
+let secondWindow;
+
 function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(
+        isDev
+            ? "http://localhost:3000"
+            : `file://${path.join(__dirname, "../build/index.html")}`
+    );
+
+    // Open the DevTools.
+    if (isDev) {
+      mainWindow.webContents.openDevTools();
     }
-  });
-
-  // and load the index.html of the app.
-  win.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
-
-  // Open the DevTools.
-  if (isDev) {
-    win.webContents.openDevTools({ mode: "detach" });
-  }
+  makeSecondWindow();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  if (isDev) {
-    installExtension(REACT_DEVELOPER_TOOLS)
-      .then(name => console.log(`Added Extension:  ${name}`))
-      .catch(error => console.log(`An error occurred: , ${error}`));
-  }
+    if (isDev) {
+        installExtension(REACT_DEVELOPER_TOOLS)
+            .then(name => console.log(`Added Extension:  ${name}`))
+            .catch(error => console.log(`An error occurred: , ${error}`));
+    }
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+const makeSecondWindow = () => {
+  secondWindow = new BrowserWindow({
+        parent: mainWindow,
+        width: 400,
+        height: 400,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            // enableRemoteModule: true
+        }
+    });
+
+  secondWindow.on('closed', () => {
+    secondWindow = null
+    });
+  secondWindow.loadURL(
+        isDev
+            ? "http://localhost:3000/Second"
+            : `file://${path.join(__dirname, "../build/index.html#Second")}`
+    );
+
+    // Open the DevTools.
+    if (isDev) {
+      secondWindow.webContents.openDevTools();
+    }
+}
+
+ipcMain.on(ipcConstants.OPEN_SECOND_WINDOW, (event) => {
+  console.log('Got models:open message');
+  if (secondWindow == null) {
+    console.log('model window is null');
+    makeSecondWindow();
+  } else {
+    // Show the existing window
+    // console.log("window is not null");
+    secondWindow.focus();
+  }
+});
